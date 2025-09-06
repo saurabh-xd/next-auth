609f9e2b8a52eb001c7dbe1ee78de611dd1919a9
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-09-06 17:20:25 +0530

    project setup and database connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose  from "mongoose";
+
+export async function connect(){
+    try {
+        mongoose.connect(process.env.MONGO_URI!) //typescript error solution->!
+        const connection = mongoose.connection
+
+        connection.on('connected', ()=>{
+            console.log('Mongodb connected');
+            
+        })
+        connection.on('error',(err)=>{
+            console.log('mongodb connection error: ' + err);
+            process.exit()
+            
+        })
+    } catch (error) {
+        console.log('Something went wrong in connecting to DB');
+        console.log(error);
+        
+        
+    }
+}
